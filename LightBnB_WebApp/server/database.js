1302f3819b1ef1e5f1462061bc4e525e3d15e8e6
YuqinHu <h878688736@gmail.com>
2023-03-09 20:25:51 -0800

    LighthouseBnB Reservations

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -78,7 +78,24 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool
+  .query(`
+  SELECT reservations.*, properties.* id, avg(property_reviews.rating) as average_rating
+  FROM reservations
+  JOIN properties ON properties.id = reservations.property_id
+  JOIN property_reviews ON properties.id = property_reviews.property_id
+  WHERE reservations.guest_id = 1
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date 
+  LIMIT $1;`, [limit]
+  )
+  .then((result) => {
+    // console.log(result.rows);
+    return result.rows;
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 }
 exports.getAllReservations = getAllReservations;
 
